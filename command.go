b693cc1c24fc2e6a0e935501ab3261bb5f77eda5
Yuri Bochkarev <baltazar.bz@gmail.com>
2019-04-02 00:37:11 +0300

    add files comparator; tests

--- command.go
+++ command.go
@@ -1,1 +1,118 @@
 package main
+
+type AdjustmentCommand interface{}
+
+type AdjustmentCommandRemoveFile struct {
+	filename string
+}
+
+type AdjustmentCommandApplyBlocksToFile struct {
+	filename string
+	blocks   []Block
+}
+
+/// Files can be compared and based on the comparison results various
+/// actions are possible, e.g. remove file from server or apply
+/// client blocks to server files.
+type FilesComparator interface {
+	Compare(
+		clientFiles []VirtualFile,
+		serverHashedFiles []HashedFile,
+	) []AdjustmentCommand
+}
+
+type filesComparator struct {
+	producerFactory ProducerFactory
+}
+
+func NewFilesComparator(producerFactory ProducerFactory) *filesComparator {
+	return &filesComparator{
+		producerFactory: producerFactory,
+	}
+}
+
+func anyContentBlocks(blocks []Block) bool {
+	for _, block := range blocks {
+		if _, ok := block.(ContentBlock); ok {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (fc *filesComparator) Compare(
+	clientFiles []VirtualFile,
+	serverHashedFiles []HashedFile,
+) []AdjustmentCommand {
+	var commands []AdjustmentCommand
+	var i, j int
+
+	addClientFile := func(i int, fastHashBlocks []Block, strongHashBlocks []Block) {
+		producer := fc.producerFactory.MakeProducer(fastHashBlocks, strongHashBlocks)
+		blocks := producer.Scan(NewStackedReadSeeker(clientFiles[i].Rw))
+		// if all blocks are hashed, the content is the same, no need to transfer anything
+		if anyContentBlocks(blocks) {
+			commands = append(commands,
+				AdjustmentCommandApplyBlocksToFile{clientFiles[i].Filename, blocks},
+			)
+		}
+	}
+
+	for i < len(clientFiles) && j < len(serverHashedFiles) {
+		if clientFiles[i].Filename < serverHashedFiles[j].Filename {
+			// new client file, add it
+			addClientFile(i, nil, nil)
+			i += 1
+		} else if clientFiles[i].Filename > serverHashedFiles[j].Filename {
+			// new server file, remove it
+			commands = append(commands,
+				AdjustmentCommandRemoveFile{serverHashedFiles[j].Filename},
+			)
+			j += 1
+		} else {
+			// file name is the same, compare contents
+			addClientFile(i, serverHashedFiles[j].FastHashes, serverHashedFiles[j].StrongHashes)
+			i += 1
+			j += 1
+		}
+	}
+
+	// add new files if any
+	for i < len(clientFiles) {
+		addClientFile(i, nil, nil)
+		i += 1
+	}
+
+	// remove server files if any
+	for j < len(serverHashedFiles) {
+		commands = append(commands,
+			AdjustmentCommandRemoveFile{serverHashedFiles[j].Filename},
+		)
+		j += 1
+	}
+
+	return commands
+}
+
+type AdjustmentCommandApplier interface {
+	Apply(
+		commands []AdjustmentCommand,
+		fs []VirtualFilesystem,
+		cr ContentReconstructor,
+	) error
+}
+
+type adjustmentCommandApplier struct{}
+
+func NewAdjustmentCommandApplier() *adjustmentCommandApplier {
+	return &adjustmentCommandApplier{}
+}
+
+func (aca *adjustmentCommandApplier) Apply(
+	commands []AdjustmentCommand,
+	fs []VirtualFilesystem,
+	cr ContentReconstructor,
+) error {
+	return nil
+}
--- command_test.go
+++ command_test.go
@@ -1,1 +1,128 @@
 package main
+
+import (
+	"crypto/md5"
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
+
+func runComparator(
+	blockSize int,
+	clientFiles []VirtualFile,
+	serverHashedFiles []HashedFile,
+) []AdjustmentCommand {
+	comparator := NewFilesComparator(NewProducerFactory(blockSize))
+	commands := comparator.Compare(clientFiles, serverHashedFiles)
+	return commands
+}
+
+func makeClientFile(filename string, content string) VirtualFile {
+	return VirtualFile{filename, strings.NewReader(content)}
+}
+
+func makeServerFile(blockSize int, filename string, content string) HashedFile {
+	fastHasher := NewMackerras(blockSize)
+	strongHasher := md5.New()
+	generator := NewHashGenerator(blockSize, fastHasher, strongHasher)
+	result := generator.Scan(strings.NewReader(content))
+	return HashedFile{
+		filename,
+		result.fastHashes,
+		result.strongHashes,
+	}
+}
+
+func TestFilesComparator_Smoke(t *testing.T) {
+	blockSize := 4
+
+	comparator := NewFilesComparator(NewProducerFactory(blockSize))
+	commands := comparator.Compare(
+		[]VirtualFile{},
+		[]HashedFile{},
+	)
+	assert.Empty(t, commands)
+}
+
+func TestFilesComparator_RemoveOneOfTwoAndChange(t *testing.T) {
+	blockSize := 4
+	clientFiles := []VirtualFile{
+		{"b", strings.NewReader("abc")},
+	}
+	serverHashedFiles := []HashedFile{
+		{"a", nil, nil},
+		{"b", nil, nil},
+	}
+	commands := runComparator(blockSize, clientFiles, serverHashedFiles)
+	assert.Len(t, commands, 2)
+	assert.Equal(t, "a", commands[0].(AdjustmentCommandRemoveFile).filename)
+	assert.Equal(t, "b", commands[1].(AdjustmentCommandApplyBlocksToFile).filename)
+	blocks := commands[1].(AdjustmentCommandApplyBlocksToFile).blocks
+	assert.Len(t, blocks, 1)
+	assert.Equal(t, []byte("abc"), blocks[0].(ContentBlock).Content())
+}
+
+func TestFilesComparator_RemoveOnlyOne(t *testing.T) {
+	blockSize := 4
+	clientFiles := []VirtualFile{
+	}
+	serverHashedFiles := []HashedFile{
+		{"a", nil, nil},
+	}
+	commands := runComparator(blockSize, clientFiles, serverHashedFiles)
+	assert.Len(t, commands, 1)
+	assert.Equal(t, "a", commands[0].(AdjustmentCommandRemoveFile).filename)
+}
+
+func TestFilesComparator_AddOneToEmpty(t *testing.T) {
+	blockSize := 4
+	clientFiles := []VirtualFile{
+		{"a", strings.NewReader("abc")},
+	}
+	serverHashedFiles := []HashedFile{
+	}
+	commands := runComparator(blockSize, clientFiles, serverHashedFiles)
+	assert.Len(t, commands, 1)
+	assert.Equal(t, "a", commands[0].(AdjustmentCommandApplyBlocksToFile).filename)
+}
+
+func TestFilesComparator_AddOneToNonEmpty(t *testing.T) {
+	blockSize := 4
+	clientFiles := []VirtualFile{
+		makeClientFile("a", "abc"),
+		makeClientFile("b", "1234"),
+	}
+	serverHashedFiles := []HashedFile{
+		makeServerFile(blockSize, "b", "1234"),
+	}
+	commands := runComparator(blockSize, clientFiles, serverHashedFiles)
+	assert.Len(t, commands, 1)
+	assert.Equal(t, "a", commands[0].(AdjustmentCommandApplyBlocksToFile).filename)
+}
+
+func TestFilesComparator_InsertAndAppendContent(t *testing.T) {
+	blockSize := 4
+	clientFiles := []VirtualFile{
+		makeClientFile("a", "123abcd"),
+		makeClientFile("b", "abcd123"),
+	}
+	serverHashedFiles := []HashedFile{
+		makeServerFile(blockSize, "a", "abcd"),
+		makeServerFile(blockSize, "b", "abcd"),
+	}
+	commands := runComparator(blockSize, clientFiles, serverHashedFiles)
+	assert.Len(t, commands, 2)
+	assert.Equal(t, "a", commands[0].(AdjustmentCommandApplyBlocksToFile).filename)
+	assert.Equal(t, "b", commands[1].(AdjustmentCommandApplyBlocksToFile).filename)
+
+	blocks1 := commands[0].(AdjustmentCommandApplyBlocksToFile).blocks
+	blocks2 := commands[1].(AdjustmentCommandApplyBlocksToFile).blocks
+
+	assert.Len(t, blocks1, 2)
+	assert.Equal(t, []byte("123"), blocks1[0].(ContentBlock).Content())
+	_ = blocks1[1].(HashedBlock)
+
+	assert.Len(t, blocks2, 2)
+	_ = blocks2[0].(HashedBlock)
+	assert.Equal(t, []byte("123"), blocks2[1].(ContentBlock).Content())
+}
